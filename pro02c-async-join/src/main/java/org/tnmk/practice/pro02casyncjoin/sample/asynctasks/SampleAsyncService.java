dc194da1416a24f59be879a41d12b452d560a9ba
trank <Kevin.Tran@pointclickcare.com>
2022-03-16 15:47:48 -0400

    Can try-catch async exception successfully.

--- pro02c-async-join/src/main/java/org/tnmk/practice/pro02casyncjoin/sample/asynctasks/SampleAsyncService.java
+++ pro02c-async-join/src/main/java/org/tnmk/practice/pro02casyncjoin/sample/asynctasks/SampleAsyncService.java
@@ -12,11 +12,15 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 public class SampleAsyncService {
+  private static final int MAX_ITEM = 3;
   private final static Logger logger = getLogger(MethodHandles.lookup().lookupClass());
 
   @Async
   public CompletableFuture<Void> asyncWriteLog(int item) {
-    //This will be run in [lTaskExecutor-3] and able to get MDC value.
+    if (item > MAX_ITEM) {
+      throw new IllegalArgumentException("Current item is " + item + ", but it cannot be greater than " + MAX_ITEM + ".");
+    }
+    //This will be run in [TaskExecutor-xxx] and able to get MDC value.
     MDC.put("asyncNanoTime", "" + System.nanoTime());
     logger.info("Sample Async Service " + item);
     return CompletableFuture.completedFuture(null);
