b58b3f5c905a5304e7cfee542ca95147b724f430
trank <Kevin.Tran@pointclickcare.com>
2022-09-11 07:56:05 -0400

    Init pro02d-async-separate-pools

---
+++ pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/Pro02dAsyncSeparatePoolsApplication.java
@@ -0,0 +1,12 @@
+package org.tnmk.practice.pro02dasyncseparatepools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro02dAsyncSeparatePoolsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Pro02dAsyncSeparatePoolsApplication.class, args);
+    }
+
+}
---
+++ pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/sample/asynctasks/SampleAsyncService.java
@@ -0,0 +1,28 @@
+package org.tnmk.practice.pro02dasyncseparatepools.sample.asynctasks;
+
+import org.slf4j.Logger;
+import org.slf4j.MDC;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.CompletableFuture;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Service
+public class SampleAsyncService {
+  public static final int MAX_ITEM = 3;
+  private final static Logger logger = getLogger(MethodHandles.lookup().lookupClass());
+
+  @Async
+  public CompletableFuture<Void> asyncWriteLog(int item) {
+    if (item > MAX_ITEM) {
+      throw new IllegalArgumentException("Current item is " + item + ", but it cannot be greater than " + MAX_ITEM + ".");
+    }
+    //This will be run in [TaskExecutor-xxx] and able to get MDC value.
+    MDC.put("asyncNanoTime", "" + System.nanoTime());
+    logger.info("Sample Async Service " + item);
+    return CompletableFuture.completedFuture(null);
+  }
+}
