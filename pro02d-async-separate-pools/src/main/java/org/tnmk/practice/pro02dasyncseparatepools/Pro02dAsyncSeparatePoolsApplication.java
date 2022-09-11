a8a85614ad10c472be119701d9c2e22e97df3350
Khoi Tran <khoi.tnm@gmail.com>
2022-09-11 09:45:21 -0400

    Spawning child threads.

--- pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/Pro02dAsyncSeparatePoolsApplication.java
+++ pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/Pro02dAsyncSeparatePoolsApplication.java
@@ -2,8 +2,10 @@ package org.tnmk.practice.pro02dasyncseparatepools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Pro02dAsyncSeparatePoolsApplication {
     public static void main(String[] args) {
         SpringApplication.run(Pro02dAsyncSeparatePoolsApplication.class, args);
--- pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/sample/SimpleController.java
+++ pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/sample/SimpleController.java
@@ -1,12 +1,14 @@
 package org.tnmk.practice.pro02dasyncseparatepools.sample;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tnmk.practice.pro02dasyncseparatepools.sample.asynctasks.simple.SimpleService;
 import org.tnmk.practice.pro02dasyncseparatepools.sample.asynctasks.spawn_children.Lv01Async;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class SimpleController {
@@ -28,7 +30,8 @@ public class SimpleController {
   public String asyncSpawnChildren(
       @RequestParam(value = "lv01children", defaultValue = "10") int lv01Children,
       @RequestParam(value = "lv02children", defaultValue = "10") int lv02Children,
-      @RequestParam(value = "lv03sleep", defaultValue = "1000") int lv03sleep) {
+      @RequestParam(value = "lv03sleep", defaultValue = "100") int lv03sleep) {
+    log.info("API /async/spawn-children is running...");
     lv01Async.spawnChildren(lv01Children, lv02Children, lv03sleep);
     return "finished";
   }
