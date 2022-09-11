c653ec6a2d4a5eb430d0fc29991081c132369d47
Khoi Tran <khoi.tnm@gmail.com>
2022-09-11 10:55:10 -0400

    Add some package-info to explain the scenarios.

---
+++ pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/sample/asynctasks/spawn_children_02_no_wait_no_stuck/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Scenario in this package:<br/>
+ * Parent thread don't need to wait for all child threads to complete.
+ * So, no matter whether there's room in pool for creating child thread or not, the parent thread just finish right away.
+ * and hence the pool have room for continue handling child threads.
+ * So it won't get stuck.
+ */
+package org.tnmk.practice.pro02dasyncseparatepools.sample.asynctasks.spawn_children_02_no_wait_no_stuck;
