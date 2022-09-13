f0b7b2080511f1848b6a079ac085fb59e325c3b5
Khoi Tran <khoi.tnm@gmail.com>
2022-09-13 15:21:29 -0400

    Add WaitNoStuckController

---
+++ pro02d-async-separate-pools/src/main/java/org/tnmk/practice/pro02dasyncseparatepools/sample/asynctasks/spawn_children_03_wait_no_stuck/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Scenario in this package:<br/>
+ * Parent thread wait for all child threads. But no room in pool for creating child thread, so parent thread get stuck, too!
+ * And hence the pool will not be able to clean up, it always full.
+ */
+package org.tnmk.practice.pro02dasyncseparatepools.sample.asynctasks.spawn_children_03_wait_no_stuck;
