561812d3ec14febc8855e21fbeccff8420f92195
Khoi Tran <khoi.tnm@gmail.com>
2022-09-21 08:41:58 -0400

    Update description.

--- pro02d-async-more-understanding/src/main/java/org/tnmk/practice/pro02dasyncmoreunderstanding/sample/asynctasks/pro02_spawn_children_03_wait_2lvs_no_stuck/Wait2LvsNoStuckController.java
+++ pro02d-async-more-understanding/src/main/java/org/tnmk/practice/pro02dasyncmoreunderstanding/sample/asynctasks/pro02_spawn_children_03_wait_2lvs_no_stuck/Wait2LvsNoStuckController.java
@@ -2,6 +2,7 @@ package org.tnmk.practice.pro02dasyncmoreunderstanding.sample.asynctasks.pro02_s
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,13 @@ public class Wait2LvsNoStuckController {
   public String asyncSpawnChildren(
       // the default lv02Count 10 will make thread pool stuck because threadPool CoreSize is only 8.
       //
+      @RequestParam(value = "requestIndex", defaultValue = "0") String requestIndex,
       @RequestParam(value = "lv02Count", defaultValue = "10") int lv02Count,
       @RequestParam(value = "lv02Sleep", defaultValue = "100") int lv02sleep) {
+    MDC.put("requestIndex", requestIndex);
     log.info("API {} is running...", REQUEST_PATH);
     wait2LvsNoStuckLv01Async.spawnChildren(lv02Count, lv02sleep);
+    MDC.remove("requestIndex");
     return "finished";
   }
 }
