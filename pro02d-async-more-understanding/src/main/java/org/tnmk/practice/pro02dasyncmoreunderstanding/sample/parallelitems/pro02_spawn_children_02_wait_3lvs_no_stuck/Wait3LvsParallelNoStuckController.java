4392042871e75386095ea4821e769d183dcc8ede
Khoi Tran <khoi.tnm@gmail.com>
2022-09-26 09:20:49 -0400

    Use stream parallel instead of async, 3lv, no stuck

---
+++ pro02d-async-more-understanding/src/main/java/org/tnmk/practice/pro02dasyncmoreunderstanding/sample/parallelitems/pro02_spawn_children_02_wait_3lvs_no_stuck/Wait3LvsParallelNoStuckController.java
@@ -0,0 +1,34 @@
+package org.tnmk.practice.pro02dasyncmoreunderstanding.sample.parallelitems.pro02_spawn_children_02_wait_3lvs_no_stuck;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class Wait3LvsParallelNoStuckController {
+  private final Wait3LvsParallelNoStuckLv01 wait3LvsParallelNoStuckLv01;
+  private static final String REQUEST_PATH = "/parallel/spawn-children/wait-3lvs-no-stuck";
+
+  @GetMapping(REQUEST_PATH)
+  public String asyncSpawnChildren(
+      // the default lv02Count 10 will make thread pool stuck because threadPool CoreSize is only 8.
+      //
+      @RequestParam(value = "lv02Count", defaultValue = "15") int lv02Count,
+      @RequestParam(value = "lv03Count", defaultValue = "2") int lv03Count,
+      @RequestParam(value = "lv03Sleep", defaultValue = "5000") int lv03sleep) throws ExecutionException, InterruptedException {
+    log.info("API {} is running...", REQUEST_PATH);
+    IntStream.range(0, 1)
+        .parallel()
+        .forEach(i ->
+            wait3LvsParallelNoStuckLv01.spawnChildren(lv02Count, lv03Count, lv03sleep)
+        );
+    return "finished";
+  }
+}
---
+++ pro02d-async-more-understanding/src/main/java/org/tnmk/practice/pro02dasyncmoreunderstanding/sample/parallelitems/pro02_spawn_children_02_wait_3lvs_no_stuck/Wait3LvsParallelNoStuckLv01.java
@@ -0,0 +1,37 @@
+package org.tnmk.practice.pro02dasyncmoreunderstanding.sample.parallelitems.pro02_spawn_children_02_wait_3lvs_no_stuck;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+import org.tnmk.practice.pro02dasyncmoreunderstanding.common.ProcessLogger;
+import org.tnmk.practice.pro02dasyncmoreunderstanding.common.ThreadLogger;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Wait3LvsParallelNoStuckLv01 {
+  private final Wait3LvsParallelNoStuckLv02 wait3LvsParallelNoStuckLv02;
+
+  @Async
+  public Future<String> spawnChildren(int lv02Count, int lv03Count, int lv03Sleep) {
+
+    String description = ProcessLogger.summary(this, null);
+    ThreadLogger.log(description, Thread.currentThread());
+
+    IntStream.range(0, lv02Count)
+        .parallel()
+        .forEach(lv02Index -> {
+          log.info(description + ": Start adding waitStuckLv02Async[" + lv02Index + "]");
+          wait3LvsParallelNoStuckLv02.spawnChildren(lv02Index, lv03Count, lv03Sleep);
+        });
+
+    log.info(description + " finished");
+    return CompletableFuture.completedFuture(description + " finished");
+  }
+}
