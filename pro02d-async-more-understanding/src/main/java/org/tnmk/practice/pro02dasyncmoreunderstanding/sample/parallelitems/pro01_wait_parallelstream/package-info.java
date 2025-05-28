fb20c56e65c7320f56a98ac01ea2f179ab941210
Khoi Tran <khoi.tnm@gmail.com>
2025-05-28 15:16:19 -0400

    Update the wrong comment, and remove redundant jmeter file.

--- pro02d-async-more-understanding/src/main/java/org/tnmk/practice/pro02dasyncmoreunderstanding/sample/parallelitems/pro01_wait_parallelstream/package-info.java
+++ pro02d-async-more-understanding/src/main/java/org/tnmk/practice/pro02dasyncmoreunderstanding/sample/parallelitems/pro01_wait_parallelstream/package-info.java
@@ -2,8 +2,6 @@
  * Scenario in this package:<br/>
  * This scenario is exactly the same as {@link org.tnmk.practice.pro02dasyncmoreunderstanding.sample.springasync.pro02_wait_async}
  * The only difference is this code use {@link java.util.stream.Stream#parallel()} instead of using {@link org.springframework.scheduling.annotation.Async}
- * And hence it won't get stuck.
- * The reason it doesn't get stuck is each {@link java.util.stream.Stream#parallel()} will create one separate ForkJoinPool, hence it won't block other pool.
- * However, the only concern is memory if we create too many pools.
+ * But the strange thing is this case won't get stuck no matter how many child threads (at both levels) are created.
  */
 package org.tnmk.practice.pro02dasyncmoreunderstanding.sample.parallelitems.pro01_wait_parallelstream;
